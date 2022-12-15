ec4ab900dbf4e331ab2e05553b5fd99be482ab38
xuan son <nguyenxuanson.kt162@gmail.com>
2022-12-15 14:23:01 +0700

    son fix shopify appbri..

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { AppProvider } from "@shopify/polaris";
+import { Link } from 'react-router-dom';
+import {
+  Provider as ShopifyProvider,
+} from "@shopify/app-bridge-react";
+
+const shop_name = window.document.getElementsByTagName("meta")["shop-name"].getAttribute("content")
+const domain = window.document.getElementsByTagName("meta")["domain"].getAttribute("content")
+const api_key = window.document.getElementsByTagName("meta")["api-key"].getAttribute("content")
+
+const host = window.btoa(domain + "/admin");
+
+const config = {
+  apiKey: api_key,
+  shopOrigin: shop_name + ".myshopify.com",
+  host: host,
+  forceRedirect: false,
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider linkComponent={Link}>
+      <ShopifyProvider config={config}>
+        <App />
+      </ShopifyProvider>
+    </AppProvider>
   </React.StrictMode>
 );
 
